89220df8451d826680b5a708195d8a548b11e213
Maik Knof <knofm@hs-weingarten.de>
2022-10-30 10:44:09 +0100

    add ros package

--- setup.bash
+++ setup.bash
@@ -1,7 +1,7 @@
 #This script will setup the repo
 
 #config
-export TAG=latest #tag for dockerfile
+export VERSION=0.0.0
 export AUTHOR='Maik Knof (@knofm)'
 export AUTHOR_EMAIL='maik.knof@rwu.de'
 export GENERATE_ROS_PACKAGE=1 #set this to 0 if you dont need a ros package
@@ -15,19 +15,27 @@ REPO=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && cd .. && pwd
 #get dir and name of the repo
 REPO_DIR=${REPO%/*}
 REPO_NAME=${REPO#*$REPO_DIR/}
-#replace "-" and " "with "_"
-ROS_REPO_NAME=$(echo $REPO_NAME | sed -e "s/-/_/g" -e "s/\ /_/g")
+#replace "-" with "_"
+ROS_REPO_NAME=$(echo $REPO_NAME | sed -i "s/-/_/g")
 
 TEMPLATE_REPO=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 #replace place-holders
 find $TEMPLATE_REPO -type f | xargs sed -i "s/\[REPO_NAME\]/${REPO_NAME}/g"
-find $TEMPLATE_REPO -type f | xargs sed -i "s/\[TAG\]/${TAG}/g"
+find $TEMPLATE_REPO -type f | xargs sed -i "s/\[VERSION\]/${VERSION}/g"
 find $TEMPLATE_REPO -type f | xargs sed -i "s/\[ROS_PACKAGE_NAME\]/${ROS_REPO_NAME}/g"
 find $TEMPLATE_REPO -type f | xargs sed -i "s/\[ROS_INTERFACES_NAME\]/${ROS_REPO_NAME}_interfaces/g"
 
 #DOCKER
 cp -r $TEMPLATE_REPO/docker $REPO/docker
+
+#ROS REPO
+if [$GENERATE_ROS_PACKAGE == '1' ]
+then
+	cp -r $TEMPLATE_REPO/ros_package $REPO/$ROS_REPO_NAME
+	mkdir -p $REPO/$ROS_REPO_NAME/src/$ROS_REPO_NAME
+	touch $REPO/$ROS_REPO_NAME/src/$ROS_REPO_NAME/__init__.py
+fi
 
 #rm template repo
 rm -rf $TEMPLATE_REPO
