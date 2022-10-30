7a032bdcdb85f3dcdabc5094f118c72ed1e3cb81
Maik Knof <knofm@hs-weingarten.de>
2022-10-30 09:53:20 +0100

    replace placehodlers and remove .git

--- setup.bash
+++ setup.bash
@@ -16,7 +16,11 @@ TEMPLATE_REPO=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && cd 
 REPO_DIR=${TEMPLATE_REPO%/*}
 REPO_NAME=${TEMPLATE_REPO#*$REPO_DIR/}
 
+#rm .git
+rm -f $TEMPLATE_REPO/.git
 
-#DOCKER
 #replace place-holders
-#sed -i 's/search_string/replace_string/' $TEMPLATE_REPO/docker/Dockerfile
+sed -i 's/[REPO_NAME]/$REPO_NAME/' $(find $TEMPLATE_REPO)
+sed -i 's/[TAG]/$TAG/' $(find $TEMPLATE_REPO)
+sed -i 's/[AUTHOR]/$AUTHOR/' $(find $TEMPLATE_REPO)
+sed -i 's/[AUTHOR_EMAIL]/$AUTHOR_EMAIL/' $(find $TEMPLATE_REPO)
