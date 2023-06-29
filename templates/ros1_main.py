73160df4354903b19f32a22fe5391d4300108b73
Maik Knof <knofm@hs-weingarten.de>
2023-06-29 17:39:11 +0200

    extend ros1_main with some usefull stuff

--- templates/ros1_main.py
+++ templates/ros1_main.py
@@ -8,7 +8,25 @@ class [PACKAGE_NAME_CAMELCASE]():
     def __init__(self):
         rospy.init_node("[ROS_PACKAGE_NAME]", anonymous=True)
         rospy.loginfo("Starting")
-        rospy.spin()
+        
+        # Publisher
+        #self.pub = rospy.Publisher(TOPIC, MSG, queue_size=10)
+
+        # Subscriber
+        #rospy.Subscriber(TOPIC, MSG, CALLBACK)
+
+        #Execute main loop
+        rate = rospy.Rate(10)
+        while not rospy.is_shutdown():
+            self.loop()
+            rate.sleep()
+
+    def loop(self):
+        '''
+        main loop
+        '''
+        rospy.loginfo('Looping')
+
 
 
 if __name__ == '__main__':
